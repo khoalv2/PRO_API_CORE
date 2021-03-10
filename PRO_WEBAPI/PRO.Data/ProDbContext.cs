cfb6aa9148c65d15623886a3454c66066c34a941
Khoalv3 <khoalv@mobix.co>
2021-03-10 09:42:42 +0700

    .net web api

---
+++ PRO_WEBAPI/PRO.Data/ProDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PRO.Core;
+using PRO.Data.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRO.Data
+{
+    public class ProDbContext : DbContext
+    {
+
+        public DbSet<Music> Musics { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+
+        public ProDbContext(DbContextOptions<ProDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new MusicConfiguration());
+            builder.ApplyConfiguration(new ArtistConfiguration());
+        }
+
+    }
+}
